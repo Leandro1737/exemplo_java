d5dc6bf56131a1923d1a40a6dcb300135df22ea7
Leandro1737 <leandromacielhti2016@gmail.com>
2024-10-04 20:45:16 -0300

    criei a classe "ALUNO"

---
+++ exercicio2/execrcicio2/src/main/java/Aluno.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leand_ud4dr0h
+ */
+public class Aluno {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    
+    Aluno(int codigo, String nome){
+            this.codigo = codigo;
+            this.nome = nome;
+}
+}
