8fc31b696aaad0ab35c98e092db00d5a7313c611
Leandro1737 <leandromacielhti2016@gmail.com>
2024-10-04 20:49:23 -0300

    Adicionei dados a classe "Curso"

--- exercicio2/execrcicio2/src/main/java/Curso.java
+++ exercicio2/execrcicio2/src/main/java/Curso.java
@@ -7,6 +7,22 @@
  *
  * @author leand_ud4dr0h
  */
-public class Curso {
+    public class Curso {
+  private String nome;
+private String duracao;
+
+Curso(String nome, String duracao){
+    this.nome = nome;
+    this.duracao = duracao;
+}
+public String verNome(){
+    return this.nome;
+}
+public String verDuracao(){
+    return this.duracao;
+}
+    
+}
+
     
 }
