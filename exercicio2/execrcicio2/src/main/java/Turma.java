506146ce40bad7618e9cd31b8270a75f986dc978
Leandro1737 <leandromacielhti2016@gmail.com>
2024-10-04 21:57:35 -0300

    Add informação a classe Turma

---
+++ exercicio2/execrcicio2/src/main/java/Turma.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leand_ud4dr0h
+ */
+public class Turma {
+     private String descricao;
+     private Curso curso;
+     
+     Turma(String descricao, Curso curso){
+     this.descricao = descricao;
+     this.curso = curso;
+             }
+}
